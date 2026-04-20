7e67dac8a96b1a8c9cd86df8ffd643cc8acd29aa
alexander.grigorev <121151393+Kato-Ya@users.noreply.github.com>
2026-04-20 15:54:48 +0700

    fix: notificationservice proto for user update: hide email sender

--- TaskService/GrpcServices/GrpcNotificationClientService.cs
+++ TaskService/GrpcServices/GrpcNotificationClientService.cs
@@ -1,24 +1,38 @@
 ﻿using NotificationService.Protos;
+using Microsoft.Extensions.Logging;
+
 
 namespace TaskService.GrpcServices;
 public class GrpcNotificationClientService
 {
     private readonly NotificationGrpc.NotificationGrpcClient _client;
+    private readonly ILogger<GrpcNotificationClientService> _logger;
 
-    public GrpcNotificationClientService(NotificationGrpc.NotificationGrpcClient client)
+    public GrpcNotificationClientService(NotificationGrpc.NotificationGrpcClient client, ILogger<GrpcNotificationClientService> logger)
     {
         _client = client;
+        _logger = logger;
     }
 
     public async Task<bool> SendTaskNotificationAsync(int userId, string message, int taskId)
     {
-        var response = await _client.SendTaskNotificationAsync(new NotificationTaskRequest
+        try
         {
-            UserId = userId,
-            Message = message,
-            TaskId = taskId
-        });
+            var response = await _client.SendTaskNotificationAsync(new NotificationTaskRequest
+            {
+                UserId = userId,
+                Message = message,
+                TaskId = taskId
+            });
 
-        return response.Success;
+            return response.Success;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Notification failed for userId={UserId}, taskId={TaskId}", userId, taskId);
+        }
+
+
+        return false;
     }
 }
